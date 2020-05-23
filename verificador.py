f2ec563151a0c6960f900e5affff9fb899b5cb9c
Fuentes <fuentesfei99@gmail.com>
2020-05-23 16:41:33 -0300

    coleta e separação de cpf e cnpj

--- verificador.py
+++ verificador.py
@@ -2,31 +2,40 @@ base = "BASEPROJETO.txt"
 
 arquivo = open(base, 'r')
 
-cnpjs = open('cnpj.txt', 'w')
-cpfs = open('cpf.txt', 'w')
+cnpjs = open('cnpj.txt', 'a')
+cpfs = open('cpf.txt', 'a')
 
-def dados_base():
+
+primeiro_verificador_pesos = [10, 9, 8, 7, 6, 5, 4, 3, 2]
+segundo_verificador_pesos = [11, 0, 9, 8, 7, 6, 5, 4, 3, 2]
+
+
+def coleta_separa():
+
     cadastros_pessoas = []
     for line in arquivo.readlines():
         cadastros_pessoas.append(line.strip(' ').strip('\n'))
+        # print(cadastros_pessoas)
 
+    for linha in cadastros_pessoas:
+        if len(linha) == 12:
+            cnpjs.write(linha + '\n')
+        elif len(linha) == 9:
+            cpfs.write(linha + '\n')
 
-def separa_Documento():
-    for linha in arquivo:
-        if len(arquivo) >= 12:
-            cnpjs.write(linha)
-        else:
-            cpfs.write(linha)
+def calculaCPF():
+    algo =[]
+    
+
 
 def main():
-    dados_base()
-    separa_Documento()
-
-
-
-
+    coleta_separa()
+    # separa_Documento()
 
 
 
 if __name__ == "__main__":
     main()
+    arquivo.close()
+    cnpjs.close()
+    cpfs.close()
