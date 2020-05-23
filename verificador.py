15a6b7d0cf5d885a3247ca1e6529c8d0456c9d8a
Fuentes <fuentesfei99@gmail.com>
2020-05-22 22:37:24 -0300

    criação de arquivo cpf e cnpj

--- verificador.py
+++ verificador.py
@@ -1,3 +1,32 @@
-arquivo = open('BASEPROJETO.txt', 'r')
+base = "BASEPROJETO.txt"
 
-print(arquivo)
+arquivo = open(base, 'r')
+
+cnpjs = open('cnpj.txt', 'w')
+cpfs = open('cpf.txt', 'w')
+
+def dados_base():
+    cadastros_pessoas = []
+    for line in arquivo.readlines():
+        cadastros_pessoas.append(line.strip(' ').strip('\n'))
+
+
+def separa_Documento():
+    for linha in arquivo:
+        if len(arquivo) >= 12:
+            cnpjs.write(linha)
+        else:
+            cpfs.write(linha)
+
+def main():
+    dados_base()
+    separa_Documento()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
